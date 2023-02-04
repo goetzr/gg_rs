9af18db902433e04b6d5e474540cadaeac01c27d
Russ Goetz <russgoetz@gmail.com>
2023-02-04 09:30:54 -0500

    WIP

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::io::{Read, Seek, SeekFrom};
 use std::path::PathBuf;
 
 use clap::Parser;
-use iced_x86::{Decoder, DecoderOptions, Formatter, Instruction, MasmFormatter};
+use iced_x86::{Decoder, DecoderOptions, Instruction};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -30,44 +30,52 @@ fn try_main() -> anyhow::Result<()> {
         _ => return Err(anyhow::Error::msg("ERROR: not a valid PE file".to_string())),
     };
 
+    // pe.sections
+    //     .iter()
+    //     .filter(|s| s.characteristics & IMAGE_SCN_CNT_CODE != 0)
+    //     .for_each(|s| {
+    //         let section_name = unsafe {
+    //             let name_bytes: Vec<_> = s.name.iter().copied().collect();
+    //             String::from_utf8_unchecked(name_bytes)
+    //         };
+    //         println!("Section name = {}", section_name);
+    //         println!("Code size = {}", s.size_of_raw_data);
+    //     });
+    // std::process::exit(0);
+
     let code_section = pe
         .sections
         .iter()
-        .filter(|s| s.characteristics & IMAGE_SCN_CNT_CODE != 0)
+        .filter(|s| s.characteristics & IMAGE_SCN_CNT_CODE != 0 && s.size_of_raw_data > 0)
         .next();
     let section = code_section.ok_or(anyhow::Error::msg("no code sections".to_string()))?;
     let section_name = unsafe {
         let name_bytes: Vec<_> = section.name.iter().copied().collect();
         String::from_utf8_unchecked(name_bytes)
     };
+    let code_file_offset = section.pointer_to_raw_data as u64;
+    let code_size = section.size_of_raw_data as usize;
     println!("Code section: {}", section_name);
+    println!("File offset = 0x{:x}", code_file_offset);
+    println!("Section size = 0x{:x}", code_size);
 
     let mut pe_file = OpenOptions::new().read(true).open(&args.pe_file)?;
-    pe_file.seek(SeekFrom::Start(section.pointer_to_raw_data as u64))?;
-    let mut code: Vec<_> = Vec::new();
-    code.reserve_exact(section.size_of_raw_data as usize);
-    pe_file.read(code.as_mut_slice())?;
+    println!("File size = 0x{:x}", pe_file.metadata().unwrap().len());
+    pe_file.seek(SeekFrom::Start(code_file_offset))?;
+    let mut code: Vec<u8> = vec![0; code_size];
+    pe_file.read(&mut code)?;
+    println!("Code size = {:x}", code.len());
 
-    print_start_of_code(TODO entry, &code[0..16]);
+    let main_addr: usize = 0x8f0;
+    analyze(main_addr as u64, &code[main_addr..main_addr + 0x62]);
     Ok(())
 }
 
-fn print_start_of_code(entry: u64, code: &[u8]) {
-    const HEXBYTES_COLUMN_BYTE_LENGTH: usize = 10;
-
-    let mut decoder =
-        Decoder::with_ip(32, code, entry, DecoderOptions::NONE);
-
-    let mut formatter = MasmFormatter::new();
-
-    // Change some options, there are many more
-    formatter.options_mut().set_first_operand_char_index(10);
-
-    // String implements FormatterOutput
-    let mut output = String::new();
+fn analyze(entry: u64, code: &[u8]) {
+    let mut decoder = Decoder::with_ip(64, code, entry, DecoderOptions::NONE);
 
     // Initialize this outside the loop because decode_out() writes to every field
-    let mut instruction = Instruction::default();
+    let mut insn = Instruction::default();
 
     // The decoder also implements Iterator/IntoIterator so you could use a for loop:
     //      for instruction in &mut decoder { /* ... */ }
@@ -78,24 +86,8 @@ fn print_start_of_code(entry: u64, code: &[u8]) {
         // There's also a decode() method that returns an instruction but that also
         // means it copies an instruction (40 bytes):
         //     instruction = decoder.decode();
-        decoder.decode_out(&mut instruction);
+        decoder.decode_out(&mut insn);
 
-        // Format the instruction ("disassemble" it)
-        output.clear();
-        formatter.format(&instruction, &mut output);
-
-        // Eg. "00007FFAC46ACDB2 488DAC2400FFFFFF     lea       rbp,[rsp-100h]"
-        print!("{:016X} ", instruction.ip());
-        let start_index = (instruction.ip() - entry) as usize;
-        let instr_bytes = &code[start_index..start_index + instruction.len()];
-        for b in instr_bytes.iter() {
-            print!("{:02X}", b);
-        }
-        if instr_bytes.len() < HEXBYTES_COLUMN_BYTE_LENGTH {
-            for _ in 0..HEXBYTES_COLUMN_BYTE_LENGTH - instr_bytes.len() {
-                print!("  ");
-            }
-        }
-        println!(" {}", output);
+        println!("{:?}", insn.mnemonic());
     }
 }
